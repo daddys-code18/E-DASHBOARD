a14b79ad2690740f3a606b5747593dcb0e6d3243
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2023-02-22 16:11:54 +0530

    signup api

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/user");
 const app = express();
-
-const connectDB = async () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
-  const productSchema = new mongoose.Schema({});
-  const product = mongoose.model("product", productSchema);
-  const data = await product.find();
-  console.warn(data);
-};
-connectDB();
-app.get("/", (req, resp) => {
-  resp.send("app is working");
+app.use(express.json());
+app.use(cors());
+app.post("/register", async (req, resp) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  resp.send(result);
 });
+
 app.listen(5000);
