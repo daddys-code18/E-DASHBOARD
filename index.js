2d2b151c6a3b1c434a3fc305b7dde1eff202e797
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2023-02-22 15:44:10 +0530

    config db

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
+const connectDB = async () => {
+  mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model("product", productSchema);
+  const data = await product.find();
+  console.warn(data);
+};
+connectDB();
 app.get("/", (req, resp) => {
   resp.send("app is working");
 });
