ca58794e0c05664e20fec4b4a26448fad8dcac9e
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2023-02-23 14:19:06 +0530

    login Api feature

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const User = require("./db/user");
 const app = express();
 app.use(express.json());
 app.use(cors());
+const port = 5000;
+
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   resp.send(result);
+});
+app.post("/login", async (req, resp) => {
+  console.warn(req.body);
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User Found" });
+    }
+  } else {
+    resp.send({ result: "No User Found" });
+  }
 });
 
 app.listen(5000);
